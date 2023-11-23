31cef429a3cab7cd2356bc1440d322e4655c13ad
Gloria Dwomoh <gloriadwomoh.contact@gmail.com>
2023-11-23 11:05:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,39 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var skills = document.getElementById("skills");
+    var skillsValue = skills.options[skills.selectedIndex].value;
+    var skillsText = skills.options[skills.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.options[skillsMember.selectedIndex].value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+
+    if (memberValue == "1") {
+        if (skillsValue == "1") {
+            if (skillsMemberValue == "1") {
+                document.getElementById("skillsMember").innerHTML = "<option value='1'>1</option>";
+            }
+            if (skillsMemberValue == "2") {
+                document.getElementById("skillsMember").innerHTML = "<option value='1'>1</option><option value
